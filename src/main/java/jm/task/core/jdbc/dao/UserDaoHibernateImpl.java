ed189d4451224349557f83ddcd053fe47ef045c9
lvv-easy <levin263825@gmail.com>
2021-07-05 17:00:49 +0300

    try 1544

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,18 +3,19 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl extends Util implements UserDao {
 
+    private SessionFactory sf = getSessionFactory();
     public UserDaoHibernateImpl() {
     }
-
     @Override
     public void createUsersTable() {
-        try(Session s = getSessionFactory().openSession()) {
+        try(Session s = sf.openSession()) {
             s.beginTransaction();
             s.createSQLQuery("create table if not exists Users (" +
                     "ID bigint primary key auto_increment," +
@@ -22,23 +23,26 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
                     "LastName varchar(15)," +
                     "Age tinyint)").executeUpdate();
         } catch (Exception e) {
+
             e.printStackTrace();
+
         }
     }
 
     @Override
     public void dropUsersTable() {
-        try(Session s = getSessionFactory().openSession()) {
+        try(Session s = sf.openSession()) {
             s.beginTransaction();
             s.createSQLQuery("drop table if exists Users").executeUpdate();
         } catch (Exception e) {
+
             e.printStackTrace();
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Session s = getSessionFactory().openSession()){
+        try (Session s = sf.openSession()){
             s.beginTransaction();
             s.save(new User(name, lastName, age));
             s.getTransaction().commit();
@@ -50,7 +54,7 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        try (Session s = getSessionFactory().openSession()){
+        try (Session s = sf.openSession()){
             s.beginTransaction();
             User u = new User();
             u.setId(id);
@@ -65,7 +69,7 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
-        try (Session s = getSessionFactory().openSession()){
+        try (Session s = sf.openSession()){
             s.beginTransaction();
             list = s.createQuery("from User").list();
         } catch (Exception e) {
@@ -76,7 +80,7 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try (Session s = getSessionFactory().openSession()){
+        try (Session s = sf.openSession()){
             s.beginTransaction();
             s.createQuery("delete User").executeUpdate();
         } catch (Exception e) {
